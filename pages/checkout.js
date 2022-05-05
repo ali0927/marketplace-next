8ae71118873167c811a432409e20383724f34c84
ali0927 <kevinali0927@gmail.com>
2022-05-05 11:02:06 +0800

    form for checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,65 +1,153 @@
-//react/next
-import React from "react";
+//react/next/packages
+import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import Cookies from "js-cookie";
+import { Controller, useForm } from "react-hook-form";
 //material ui
 import {
-  Button,
   List,
   ListItem,
-  TextField,
   Typography,
+  TextField,
+  Button,
 } from "@material-ui/core";
-//component
+//components
 import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
 import useStyles from "../utils/styles";
+import { MarketplaceContext } from "../utils/MarketplaceContext";
 
-function Checkout() {
+export default function Checkout() {
+  const { currentAccount } = useContext(MarketplaceContext);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    // setValue,
+  } = useForm();
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  // const {
+  //   userInfo,
+  //   cart: { shippingAddress },
+  // } = state;
+  useEffect(() => {
+    // setValue('fullName', shippingAddress.fullName);
+    // setValue('address', shippingAddress.address);
+    // setValue('city', shippingAddress.city);
+    // setValue('postalCode', shippingAddress.postalCode);
+    // setValue('country', shippingAddress.country);
+  }, []);
+
   const classes = useStyles();
+  const submitHandler = ({ discordId, ethAddress, physicalAddress, email }) => {
+    dispatch({
+      type: "SAVE_USER_DETAILS",
+      payload: { discordId, ethAddress, physicalAddress, email },
+    });
+    Cookies.set("userDetails", {
+      discordId,
+      ethAddress,
+      physicalAddress,
+      email,
+    });
+    router.push("/payment");
+  };
   return (
-    <Layout title="Checkout">
-      <form className={classes.form}>
+    <Layout title="Shipping Address">
+      <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
         <Typography component="h1" variant="h1">
           Checkout
         </Typography>
         <List>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="ethaddress"
-              label="Eth Address"
-              inputProps={{ type: "ethaddress" }}
-            ></TextField>
+            <Controller
+              name="discordId"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="discordId"
+                  label="Discord ID"
+                  error={Boolean(errors.discordId)}
+                  helperText={
+                    errors.discordId
+                      ? errors.discordId.type === "minLength"
+                        ? "Discord ID length is more than 1"
+                        : "Discord ID is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="physicaladdress"
-              label="Physical Address"
-              inputProps={{ type: "physicaladdress" }}
-            ></TextField>
+            <Controller
+              name="physicalAddress"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 2,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="physicalAddress"
+                  label="Physical Address"
+                  error={Boolean(errors.physicalAddress)}
+                  helperText={
+                    errors.physicalAddress
+                      ? errors.physicalAddress.type === "minLength"
+                        ? "Physical address length is more than 1"
+                        : "Physical address is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="discordid"
-              label="Discord Id"
-              inputProps={{ type: "discordid" }}
-            ></TextField>
-          </ListItem>
-          <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="email"
-              label="Email"
-              inputProps={{ type: "email" }}
-            ></TextField>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  inputProps={{ type: "email" }}
+                  error={Boolean(errors.email)}
+                  helperText={
+                    errors.email
+                      ? errors.email.type === "pattern"
+                        ? "Email is not valid"
+                        : "Email is required"
+                      : ""
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
-              Check Out
+              Continue
             </Button>
           </ListItem>
         </List>
@@ -67,5 +155,3 @@ function Checkout() {
     </Layout>
   );
 }
-
-export default Checkout;
