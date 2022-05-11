95382ab5f5d8a9b8f02a026cbf6c6275dbba8c7b
ali0927 <kevinali0927@gmail.com>
2022-05-11 11:54:04 +0800

    fix: ETH Listener

--- listener/eth.js
+++ listener/eth.js
@@ -1,100 +1,114 @@
-const ethers = require("ethers")
-const mongoose = require("mongoose")
-const EscrowWallet = require("../lib/contracts/EscrowWallet.json")
-const MockSho = require("../lib/contracts/MockSho.json")
-const Wallet = require("../models/Wallet.model")
-const BurnEvent = require("../models/BurnEvent.model")
+const ethers = require('ethers')
+const mongoose = require('mongoose')
+const EscrowWallet = require('../lib/contracts/EscrowWallet.json')
+const MockSho = require('../lib/contracts/MockSho.json')
+const Wallet = require('../models/Wallet.model')
+const BurnEvent = require('../models/BurnEvent.model')
 
-require("dotenv").config()
+require('dotenv').config()
 
 let CHAINID_ETH =
-  process.env.NODE_ENV === "prod"
+  process.env.NODE_ENV === 'prod'
     ? process.env.CHAINID_ETH
-    : process.env.CHAINID_ETH_TEST;
+    : process.env.CHAINID_ETH_TEST
 
 const ethProvider = new ethers.providers.JsonRpcProvider(
   process.env.RPC_ETHEREUM
-);
+)
 
 const EscrowWalletContract = new ethers.Contract(
   EscrowWallet.address[CHAINID_ETH],
   EscrowWallet.abi,
   ethProvider
-);
+)
 
-EscrowWalletContract.on('BurnToken', async (user, token, amount_bn, nonce_bn, event) => {
-  console.log('Eth BurnToken---', user, token, amount_bn)
-  try {
-    const nonce = nonce_bn.toNumber()  
-    const address = ethers.utils.getAddress(user)
-    const tokenAddress = ethers.utils.getAddress(token)
-    const amount = parseFloat(ethers.utils.formatEther(amount_bn))
+EscrowWalletContract.on(
+  'BurnToken',
+  async (user, token, amount_bn, nonce_bn, event) => {
+    console.log('Eth BurnToken---', user, token, amount_bn)
+    try {
+      const nonce = nonce_bn.toNumber()
+      const address = ethers.utils.getAddress(user)
+      const tokenAddress = ethers.utils.getAddress(token)
+      const amount = parseFloat(ethers.utils.formatEther(amount_bn))
 
-    const TokenContract = new ethers.Contract(tokenAddress, MockSho.abi, ethProvider)
-    const tokenSymbol = await TokenContract.symbol()
-    const tokenName = await TokenContract.name()
+      const TokenContract = new ethers.Contract(
+        tokenAddress,
+        MockSho.abi,
+        ethProvider
+      )
+      const tokenSymbol = await TokenContract.symbol()
+      const tokenName = await TokenContract.name()
 
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
 
-    let burnEvent
-    burnEvent = await BurnEvent.findOne({
-      nonce: nonce,
-      chainId: CHAINID_ETH
-    })
-
-    if (burnEvent) {
-      return
-    }
-    else {
-      burnEvent = await BurnEvent.create({
+      let burnEvent
+      burnEvent = await BurnEvent.findOne({
         nonce: nonce,
         chainId: CHAINID_ETH,
-      });
+      })
 
       if (burnEvent) {
-        return;
+        return
       } else {
         burnEvent = await BurnEvent.create({
           nonce: nonce,
           chainId: CHAINID_ETH,
-          user: address,
-          token: tokenAddress,
-          amount: amount,
-        });
+        })
+
+        if (burnEvent) {
+          return
+        } else {
+          burnEvent = await BurnEvent.create({
+            nonce: nonce,
+            chainId: CHAINID_ETH,
+            user: address,
+            token: tokenAddress,
+            amount: amount,
+          })
+        }
       }
-      else {
-        data.balances.push({
+
+      let data
+      data = await Wallet.findOne({ address: address })
+
+      if (data) {
+        const tokenIdx = data.balances.findIndex(
+          (item) => item.token_address === tokenAddress
+        )
+        if (tokenIdx != -1) {
+          data.balances[tokenIdx].balance += amount
+        } else {
+          data.balances.push({
             token_address: tokenAddress,
             token_name: tokenName,
             token_symbol: tokenSymbol,
-            balance: amount
+            balance: amount,
+          })
+        }
+        data = await Wallet.findOneAndUpdate({ _id: data._id }, data, {
+          new: true,
+        })
+      } else {
+        data = await Wallet.create({
+          address: address,
+          balances: [
+            {
+              token_address: tokenAddress,
+              token_name: tokenName,
+              token_symbol: tokenSymbol,
+              balance: amount,
+            },
+          ],
         })
       }
-      data = await Wallet.findOneAndUpdate(
-        { _id: data._id },
-        data,
-        { new: true }
-      )
-    }
-    else {
-       data = await Wallet.create({
-        address: address, 
-        balances: [
-          {
-            token_address: tokenAddress,
-            token_name: tokenName,
-            token_symbol: tokenSymbol,
-            balance: amount
-          }
-        ]
-      });
-    }
 
-    await mongoose.disconnect()
-  } catch (error) {
-    console.log(error)
+      await mongoose.disconnect()
+    } catch (error) {
+      console.log(error)
+    }
   }
-})
+)
